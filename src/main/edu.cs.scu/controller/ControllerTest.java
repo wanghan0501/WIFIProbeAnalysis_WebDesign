236bb5686d2a7754b60f40b3cb06aa7508d13fc4
mai ciu s <maicius@outlook.com>
2017-03-30 17:22:25 +0800

    spring + mvc整合完成

---
+++ src/main/edu.cs.scu/controller/ControllerTest.java
@@ -0,0 +1,11 @@
+package edu.cs.scu.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by maicius on 2017/3/30.
+ */
+@Controller
+public class ControllerTest {
+
+}
---
+++ src/main/edu.cs.scu/service/ServiceEg.java
@@ -0,0 +1,9 @@
+package edu.cs.scu.service;
+
+/**
+ * Created by maicius on 2017/3/30.
+ */
+public interface ServiceEg {
+    public String test();
+
+}
