9edb36c737826ecc27f02db440bad0dab23fbc81
mai ciu s <maicius@outlook.com>
2017-05-07 02:34:26 +0800

    测试数据库连接（10000个没问题）

--- src/main/java/com/cs/scu/controller/DataUploadController.java
+++ src/main/java/com/cs/scu/controller/DataUploadController.java
@@ -1,6 +1,8 @@
 package com.cs.scu.controller;
 
+import com.cs.scu.entity.User;
 import com.cs.scu.service.DataUploadService;
+import com.cs.scu.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,7 +12,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.net.URLEncoder;
 
 /**
  * Created by lch on 2017/5/3.
@@ -19,6 +20,11 @@ import java.net.URLEncoder;
 public class DataUploadController {
     @Autowired
     private DataUploadService dataUploadService;
+
+    @Autowired
+    private LoginService loginService;
+    User user = new User();
+
     @RequestMapping(value = "/upload",method = RequestMethod.POST,produces="text/html;charset=UTF-8")
     @ResponseBody
     public Object upload(@RequestBody String json) {
@@ -31,7 +37,16 @@ public class DataUploadController {
             e.printStackTrace();
         }
         try {
-            System.out.print(res);
+            System.out.println(res);
+            user.setUserName("maicius");
+            user.setPassword("110110");
+            System.out.println(user.getUserName());
+            User loginUser = loginService.doUserLogin(user);
+            if(loginUser !=null)
+                System.out.println("success!!!");
+            else{
+                System.out.println("failed");
+            }
         }catch (Exception e) {
             e.printStackTrace();
         }
---
+++ src/main/java/com/cs/scu/mapper/UploadController.java
@@ -0,0 +1,7 @@
+package com.cs.scu.mapper;
+
+/**
+ * Created by maicius on 2017/5/7.
+ */
+public class UploadController {
+}
